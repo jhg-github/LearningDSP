ca6973e44e3532a817d8ec72feaa6c1053a9e03a
javi <javi@email.com>
2021-06-27 21:40:08 +0200

    added fft of filtered signal

--- python/filter_response.py
+++ python/filter_response.py
@@ -6,10 +6,11 @@ from scipy import signal
 
 def SingleSidedFFT(y, fs):
     """ len(y) must be even """
+    l = len(y)
     Y = np.fft.fft(y)               # fft
     Y = np.abs(Y)                   # abs to get magnitude
-    Y = Y/L                         # to scale it back
-    Y = Y[:L//2+1]                  # only positive frequencies
+    Y = Y/l                         # to scale it back
+    Y = Y[:l//2+1]                  # only positive frequencies
     Y[1:] = Y[1:]*2                 # scale for single sided spectrum
     l = len(y)
     f = np.arange(0,l//2+1)*fs/l    # from 0 to Nyquist
@@ -17,7 +18,7 @@ def SingleSidedFFT(y, fs):
 
 
 # constants
-L = 20
+L = 30
 FS_Hz = 1000
 print('Frequency resolution: {}Hz'.format(FS_Hz/L) )
 
@@ -45,19 +46,28 @@ filt2 = np.ones(20)/20  # this will have a null at 50Hz for FS=1000Hz
 w2, H2 = signal.freqz(filt2, fs=FS_Hz, worN=501, include_nyquist=True)
 H2 = abs(H2)
 
-# print(f_filt, Y_filt)
-# movavg_filter_fft = fft( movavg_filter, 32 )
-# input_signal = scipy.array([2,1,1,1,1,1])
-# filtered_signal = signal.convolve(input_signal, movavg_filter)
+# signal filtered
+filtered_signal1 = signal.convolve(sig, filt1)
+filtered_signal1 = filtered_signal1[0:L]
+f_sig_filt1, Y_sig_filt1 = SingleSidedFFT(filtered_signal1, FS_Hz)
+
+filtered_signal2 = signal.convolve(sig, filt2)
+filtered_signal2 = filtered_signal2[0:L]
+f_sig_filt2, Y_sig_filt2 = SingleSidedFFT(filtered_signal2, FS_Hz)
+
 
 # plots
-fig, axes = plt.subplots(2, 2)
+fig, axes = plt.subplots(3, 2)
 axes[0][0].stem(t, sig, use_line_collection=True, label ='signal')
 axes[0][1].stem(f_sig, Y_sig, use_line_collection=True, label ='signal FFT')
 axes[1][0].stem(filt1, use_line_collection=True, label ='filter1')
 axes[1][0].stem(filt2, use_line_collection=True, label ='filter2')
 axes[1][1].plot(w1, H1, '.', label ='filter1 FFT')
 axes[1][1].plot(w2, H2, '.', label ='filter2 FFT')
+axes[2][0].plot(t, filtered_signal1, '.', label ='signal filtered 1')
+axes[2][0].plot(t, filtered_signal2, '.', label ='signal filtered 2')
+axes[2][1].plot(f_sig_filt1, Y_sig_filt1, '.', label ='signal filtered 1 FFT')
+axes[2][1].plot(f_sig_filt2, Y_sig_filt2, '.', label ='signal filtered 2 FFT')
 for ax_r in axes:
     for ax_c in ax_r:
         ax_c.legend()

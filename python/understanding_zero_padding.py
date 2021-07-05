5d730abc7ba30a422613c7aa6441f9b1af3da96b
javi <javi@email.com>
2021-07-05 16:50:01 +0200

    added pading

--- python/understanding_zero_padding.py
+++ python/understanding_zero_padding.py
@@ -3,21 +3,39 @@ import matplotlib.pyplot as plt
 
 ORIGINAL_L = 16
 k = 3
-N = 16
+N = ORIGINAL_L
+
+
 n = np.arange(ORIGINAL_L)
 sig = np.sin(2*np.pi*k*n/N)
 
+sig_dft_len = (len(sig)//2)+1
+sig_dft_freqs = np.arange(sig_dft_len)
+sig_dft_mag = np.fft.fft(sig)       # fft
+sig_dft_mag = np.abs(sig_dft_mag)   # abs to get magnitude  # no amplitude compensation
+sig_dft_mag = sig_dft_mag[0:sig_dft_len]
+
+sig_pad16 = np.pad(sig, (0,16))
+sig_pad16_dft_len = (len(sig_pad16)//2)+1
+sig_pad16_dft_freqs = np.arange(sig_pad16_dft_len)
+sig_pad16_dft_mag = np.fft.fft(sig_pad16)       # fft
+sig_pad16_dft_mag = np.abs(sig_pad16_dft_mag)   # abs to get magnitude  # no amplitude compensation
+sig_pad16_dft_mag = sig_pad16_dft_mag[0:sig_pad16_dft_len]
+
+sig_pad128 = np.pad(sig, (0,112))
+sig_pad128_dft_len = (len(sig_pad128)//2)+1
+sig_pad128_dft_freqs = np.arange(sig_pad128_dft_len)
+sig_pad128_dft_mag = np.fft.fft(sig_pad128)       # fft
+sig_pad128_dft_mag = np.abs(sig_pad128_dft_mag)   # abs to get magnitude  # no amplitude compensation
+sig_pad128_dft_mag = sig_pad128_dft_mag[0:sig_pad128_dft_len]
+
 fig, axes = plt.subplots(3, 2)
-axes[0][0].plot(n, sig, '.', label ='signal')
-# axes[0][1].stem(f_sig, Y_sig, use_line_collection=True, label ='signal FFT')
-# axes[1][0].stem(filt1, use_line_collection=True, label ='filter1')
-# axes[1][0].stem(filt2, use_line_collection=True, label ='filter2')
-# axes[1][1].plot(w1, H1, '.', label ='filter1 FFT')
-# axes[1][1].plot(w2, H2, '.', label ='filter2 FFT')
-# axes[2][0].plot(t, filtered_signal1, '.', label ='signal filtered 1')
-# axes[2][0].plot(t, filtered_signal2, '.', label ='signal filtered 2')
-# axes[2][1].plot(f_sig_filt1, Y_sig_filt1, '.', label ='signal filtered 1 FFT')
-# axes[2][1].plot(f_sig_filt2, Y_sig_filt2, '.', label ='signal filtered 2 FFT')
+axes[0][0].plot(sig, '.-', label ='sig')
+axes[0][1].plot(sig_dft_freqs, sig_dft_mag, '.-', label ='sig_dft_mag')
+axes[1][0].plot(sig_pad16, '.-', label ='sig_pad16')
+axes[1][1].plot(sig_pad16_dft_freqs, sig_pad16_dft_mag, '.-', label ='sig_pad16_dft_mag')
+axes[2][0].plot(sig_pad128, '.-', label ='sig_pad128')
+axes[2][1].plot(sig_pad128_dft_freqs, sig_pad128_dft_mag, '.-', label ='sig_pad128_dft_mag')
 for ax_r in axes:
     for ax_c in ax_r:
         ax_c.legend()

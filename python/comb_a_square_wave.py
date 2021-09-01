ce314fee939aa19b6bce98f02776e7dbebe97288
JavierMJK <javier.garcia@xyleminc.com>
2021-09-01 11:31:35 +0200

    added comb filters

--- python/comb_a_square_wave.py
+++ python/comb_a_square_wave.py
@@ -15,32 +15,78 @@ def SingleSidedFFT(y, fs):
     f = np.arange(0,l//2+1)*fs/l    # from 0 to Nyquist
     return f, Y
 
+def SquareSignal(f, fs, number_periods):
+    period_len = fs // f
+    high = np.ones(period_len // 2)
+    low = np.ones(period_len // 2) * -1
+    # square = np.concatenate((high,low))
+    square = np.empty(0)
+    for i in range(number_periods):
+        square = np.concatenate((square,high))
+        square = np.concatenate((square,low))
+    t = np.arange(number_periods * period_len) / fs
+    return t, square
 
 # constants
 FS_Hz = 800
 L = FS_Hz
 print('Frequency resolution: {}Hz'.format(FS_Hz/L) )
 
-# square signal
+# ideal square signal
 F0 = 10
-t = np.linspace(0, 1, FS_Hz, endpoint=False)
-sig = signal.square(2 * np.pi * F0 * t)
-f_sig, Y_sig = SingleSidedFFT(sig, FS_Hz)
-Y_sig_dB = 20 * np.log10(Y_sig)
+PERIODS = 10
+t, sig_ideal = SquareSignal(F0, FS_Hz, PERIODS)
+f_sig_ideal, Y_sig_ideal = SingleSidedFFT(sig_ideal, FS_Hz)
+Y_sig_ideal_dB = 20 * np.log10(Y_sig_ideal)
+
+# real square signal
+offset = -4
+gain = 1
+sig_real = sig_ideal + (gain*t) + offset
+f_sig_real, Y_sig_real = SingleSidedFFT(sig_real, FS_Hz)
+
+# comb filter
+comb1=np.zeros(L)
+comb1[0] = 1
+comb1[40] = -1
+print(comb1)
+f_comb1, Y_comb1 = SingleSidedFFT(comb1, FS_Hz)
+
+# signal filtered comb1
+sig_filt1 = signal.convolve(sig_real, comb1) / 2
+sig_filt1 = sig_filt1[0:L]
+f_sig_filt1, Y_sig_filt1 = SingleSidedFFT(sig_filt1, FS_Hz)
+
+# signal filtered comb2
+sig_filt2 = signal.convolve(sig_filt1, comb1) / 2
+sig_filt2 = sig_filt2[0:L]
+f_sig_filt2, Y_sig_filt2 = SingleSidedFFT(sig_filt2, FS_Hz)
+
+# signal filtered comb3
+sig_filt3 = signal.convolve(sig_filt2, comb1) / 2
+sig_filt3 = sig_filt2[0:L]
+f_sig_filt3, Y_sig_filt3 = SingleSidedFFT(sig_filt3, FS_Hz)
 
 # plots
 fig, axes = plt.subplots(3, 2)
-axes[0][0].plot(t, sig, '.', label ='signal')
-axes[0][1].plot(f_sig, Y_sig, label ='signal FFT')
-# axes[0][1].plot(f_sig, Y_sig_dB, label ='signal FFT dB')
-# axes[1][0].stem(filt1, use_line_collection=True, label ='filter1')
-# axes[1][0].stem(filt2, use_line_collection=True, label ='filter2')
-# axes[1][1].plot(w1, H1, '.', label ='filter1 FFT')
-# axes[1][1].plot(w2, H2, '.', label ='filter2 FFT')
-# axes[2][0].plot(t, filtered_signal1, '.', label ='signal filtered 1')
-# axes[2][0].plot(t, filtered_signal2, '.', label ='signal filtered 2')
-# axes[2][1].plot(f_sig_filt1, Y_sig_filt1, '.', label ='signal filtered 1 FFT')
-# axes[2][1].plot(f_sig_filt2, Y_sig_filt2, '.', label ='signal filtered 2 FFT')
+axes[0][0].plot(t, sig_ideal, '.-', label ='ideal')
+axes[0][0].plot(t, sig_real, '.-', label ='real')
+
+axes[0][1].plot(f_sig_ideal, Y_sig_ideal, label ='ideal FFT')
+axes[0][1].plot(f_sig_real, Y_sig_real, label ='real FFT')
+
+axes[1][1].plot(f_comb1, Y_comb1, label ='comb1 FFT')
+
+axes[2][0].plot(t, sig_filt1, '.-', label ='filt1')
+# axes[2][0].plot(t, sig_filt2, '.-', label ='filt2')
+# axes[2][0].plot(t, sig_filt3, '.-', label ='filt3')
+
+axes[2][1].plot(f_sig_ideal, Y_sig_ideal, label ='ideal FFT')
+axes[2][1].plot(f_sig_filt1, Y_sig_filt1, label ='filt1 FFT')
+axes[2][1].plot(f_sig_filt2, Y_sig_filt2, label ='filt2 FFT')
+axes[2][1].plot(f_sig_filt3, Y_sig_filt3, label ='filt3 FFT')
+
+
 for ax_r in axes:
     for ax_c in ax_r:
         ax_c.legend()
